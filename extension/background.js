2d42c75f9b1a128ebf9f91ede258fca60964af41
Sutas Nakasawek <sutas@arterex.com>
2019-09-30 17:30:38 +0700

    Make browser button compatible with chrome Make in-page button runnable on chrome (not sure if this break firefox compat)

--- extension/background.js
+++ extension/background.js
@@ -1,7 +1,11 @@
+let this_browser = null;
+if(!this_browser) try { this_browser = browser; } catch(e){}
+if(!this_browser)try { this_browser = chrome; } catch(e) {}
+
 function openPage() {
-    browser.tabs.create({
+    this_browser.tabs.create({
         url: "https://steamcommunity.com/my/gcpd/570/?category=Account&tab=MatchPlayerReportIncoming"
     });
 }
 
-browser.browserAction.onClicked.addListener(openPage);
+this_browser.browserAction.onClicked.addListener(openPage);
