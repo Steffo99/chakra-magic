6ceb27b9c9247d93136942418ab786a2709f9e6a
Stefano Pigozzi <ste.pigozzi@gmail.com>
2019-10-02 10:48:18 +0200

    Formatting changes

--- extension/background.js
+++ extension/background.js
@@ -1,11 +1,11 @@
-let this_browser = null;
-if(!this_browser) try { this_browser = browser; } catch(e){}
-if(!this_browser)try { this_browser = chrome; } catch(e) {}
+let thisBrowser = null;
+if(!thisBrowser) try { thisBrowser = browser; } catch(e) {}
+if(!thisBrowser) try { thisBrowser = chrome; } catch(e) {}
 
 function openPage() {
-    this_browser.tabs.create({
+    thisBrowser.tabs.create({
         url: "https://steamcommunity.com/my/gcpd/570/?category=Account&tab=MatchPlayerReportIncoming"
     });
 }
 
-this_browser.browserAction.onClicked.addListener(openPage);
+thisBrowser.browserAction.onClicked.addListener(openPage);
